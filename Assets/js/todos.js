ded0e6ac6d07a8001484ee5f11c2deed0dcde22e
TP <tungint@gmail.com>
2017-01-07 14:54:51 -0500

    activate input[text]

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,10 +1,24 @@
+// check off todo List by clicking
 $("li").click(function() {
   $(this).toggleClass("completed");
 });
 
+// click X to fade out then remove a Todo
 $("span").click(function(event){
   $(this).parent().fadeOut(1000, function(){
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+// grab a Todo Text from input
+$("input").keypress(function(event){
+  // Enter(13) hit by user
+  if (event.which === 13) {
+    var newTodo = $(this).val();
+    // clear out input, it acts as a setter rather than a getter
+    $(this).val("");
+    // append newTodo to ul
+    $("ul").append("<li><span>X</span> " + newTodo + "</li>");
+  }
 });
